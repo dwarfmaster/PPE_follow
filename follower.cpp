7919274ffaf5cc647471962baf2000d1e040a05c
lucas8 <luc.linux@mailoo.org>
2014-02-23 17:30:38 +0100

    Added sliders to dynamically adjust detection.

--- follower.cpp
+++ follower.cpp
@@ -3,7 +3,15 @@ using namespace cv;
 
 Follower::Follower()
     : m_st(Unknown)
-{}
+{
+    namedWindow("Binary");
+    createTrackbar("min hue",        "Binary", NULL, 180);
+    createTrackbar("max hue",        "Binary", NULL, 180);
+    createTrackbar("min saturation", "Binary", NULL, 255);
+    createTrackbar("max saturation", "Binary", NULL, 255);
+    createTrackbar("min value",      "Binary", NULL, 255);
+    createTrackbar("max value",      "Binary", NULL, 255);
+}
 
 Follower::~Follower()
 {}
@@ -18,8 +26,14 @@ void Follower::process(const cv::Mat& img)
     for(int i = 0; i < img.rows; ++i) {
         for(int j = 0; j < img.cols; ++j) {
             auto vec = hsv.at<Vec<unsigned char,3>>(i,j);
-            if(vec[1] >= 180 && vec[2] >= 100
-                    && (vec[0] < 10 || vec[0] > 170))
+
+            if(vec[0] > getTrackbarPos("min hue", "Binary")
+                    && vec[0] < getTrackbarPos("max hue", "Binary")
+                    && vec[1] > getTrackbarPos("min saturation", "Binary")
+                    && vec[1] < getTrackbarPos("max saturation", "Binary")
+                    && vec[2] > getTrackbarPos("min value", "Binary")
+                    && vec[2] < getTrackbarPos("max value", "Binary")
+                    )
                 binary.at<unsigned char>(i,j) = 255;
             else
                 binary.at<unsigned char>(i,j) = 0;
